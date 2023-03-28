b3e632212a672ae52047db4c1c991753bb018d8c
Amir Khasru <muhammadamirkhasru@gmail.com>
2023-03-29 02:11:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+const singer = {name: 'Dr Mahfuz Rahman', job: 'Singer'}
 
+const singerStyle = {
+  color: 'skyblue',
+  backgroundColor: 'gray',
+  padding : 20 ,
+  radius: 20 ,
+
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello Dude. React. How are you?</h3>
+          <p style={singerStyle}>
+            Name: {singer.name} <br />
+            Job: {singer.job}
+          </p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
